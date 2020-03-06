a22c16f437067768beb33bff023b059ca244aeda
luyongmin <1329852465@qq.com>
2020-03-06 21:55:14 +0800

    update

---
+++ src/main/java/com/luyongmin/task/demotask/controller/LogController.java
@@ -0,0 +1,10 @@
+package com.luyongmin.task.demotask.controller;
+
+/**
+ * @program: demotask
+ * @author: lu.yongmin
+ * @create: 2020-03-06 21:15
+ * @description:
+ **/
+public class LogController {
+}
